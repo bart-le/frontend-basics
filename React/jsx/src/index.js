6e7bf2cf3b6a9b88c7907f876e612154f9dcc0d6
Bart Le <le.bartlomiej@gmail.com>
2019-02-27 00:42:34 +0100

    Fix JSX

--- React/jsx/src/index.js
+++ React/jsx/src/index.js
@@ -7,9 +7,11 @@ import ReactDOM from 'react-dom';
 const App = () => {
   return (
     <div>
-      <label for="name" class="label"></label>
+      <label for="name" class="label">
+        Enter name:
+      </label>
       <input id="name" type="text" />
-      <button style="background-color: red; color: white;">Submit</button>
+      <button style={{ backgroundColor: 'red', color: 'white' }}>Submit</button>
     </div>
   );
 };
