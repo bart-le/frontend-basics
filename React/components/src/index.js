79a9bac2aafff24b6d12da7df271de873f606897
Bart Le <le.bartlomiej@gmail.com>
2019-02-27 21:09:42 +0100

    Add attribute

--- React/components/src/index.js
+++ React/components/src/index.js
@@ -12,16 +12,19 @@ const App = () => {
         author="Huey"
         timeAgo="Today at 4:45PM"
         content="Nice blog post!"
+        avatar={faker.image.avatar()}
       />
       <CommentDetails
         author="Dewey"
         timeAgo="Today at 2:00AM"
         content="I like the subject."
+        avatar={faker.image.avatar()}
       />
       <CommentDetails
         author="Louie"
         timeAgo="Yesterday at 5:00PM"
         content="Amazing writing."
+        avatar={faker.image.avatar()}
       />
     </div>
   );
