2a20b2d6b9fe4b09bd37ba7333cffc47bf9c6cf9
Bart Le <le.bartlomiej@gmail.com>
2019-02-27 19:25:04 +0100

    Import component

--- React/components/src/index.js
+++ React/components/src/index.js
@@ -3,6 +3,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 
+import CommentDetails from './CommentDetails';
+
 const App = () => {
   return (
     <div className="ui container comments">
