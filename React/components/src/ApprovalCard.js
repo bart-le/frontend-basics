8c3ac214de40987f755c12c0f47cee3b8783ccf1
Bart Le <le.bartlomiej@gmail.com>
2019-02-28 11:40:22 +0100

    Update component

--- React/components/src/ApprovalCard.js
+++ React/components/src/ApprovalCard.js
@@ -2,7 +2,22 @@
 import React from 'react';
 
 const ApprovalCard = () => {
-
+  return (
+    <div className="ui card">
+      <div className="content">Are you sure?
+        <div className="extra content">
+          <div class="ui two buttons">
+            <div className="ui basic green button">
+              Approve
+            </div>
+            <div className="ui basic red button">
+              Reject
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ApprovalCard;
