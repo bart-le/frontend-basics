fd635e5e0283be4a4a5b7531443186e7c61e4530
Bart Le <le.bartlomiej@gmail.com>
2019-03-01 20:18:30 +0100

    Initialize state with constructor method

--- React/seasons/src/index.js
+++ React/seasons/src/index.js
@@ -3,6 +3,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      lat: null,
+      errorMessage: '',
+    };
+  };
+  
   render() {
     window.navigator.geolocation.getCurrentPosition (
       position => console.log(position),
