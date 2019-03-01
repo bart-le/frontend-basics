8ba8ae4728debf35c73eca2641e7686c5e86f032
Bart Le <le.bartlomiej@gmail.com>
2019-03-01 21:08:59 +0100

    Add render conditions

--- React/seasons/src/index.js
+++ React/seasons/src/index.js
@@ -25,12 +25,16 @@ class App extends React.Component {
     );
   };
   
-  render() {
-    return (
-      <div>
-        Latitude:
-      </div>
-    );
+  render() { 
+    if (this.state.lat && !this.state.errorMessage) {
+      return <div>Latitude: {this.state.lat}</div>;
+    };
+
+    if (!this.state.lat && this.state.errorMessage) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    };
+
+    return <div>Loading...</div>;
   };
 };
 
