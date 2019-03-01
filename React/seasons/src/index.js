16eb5e62b096c61f0bb4407ca2ee080afb3007a4
Bart Le <le.bartlomiej@gmail.com>
2019-03-01 20:50:17 +0100

    Set states for geolocation call

--- React/seasons/src/index.js
+++ React/seasons/src/index.js
@@ -12,8 +12,16 @@ class App extends React.Component {
     };
 
     window.navigator.geolocation.getCurrentPosition (
-      position => console.log(position),
-      err => console.log(err)
+      position => {
+        this.setState({
+          lat: position.coords.latitude,
+        });
+      },
+      err => {
+        this.setState({
+          errorMessage: err.message,
+        });
+      }
     );
   };
   
