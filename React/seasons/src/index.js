4f3630ee22e21dc8ed71d78e85e2636d2fd1c4a0
Bart Le <le.bartlomiej@gmail.com>
2019-03-02 18:45:04 +0100

    Import SeasonDisplay component

--- React/seasons/src/index.js
+++ React/seasons/src/index.js
@@ -2,6 +2,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SeasonDisplay from './SeasonDisplay';
+
 class App extends React.Component {
   state = { lat: null, errorMessage: '' };
 
