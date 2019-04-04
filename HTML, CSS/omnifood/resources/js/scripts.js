c1639c1979173dcbf4d16cb5436b1f14af470ae7
Bart Le <le.bartlomiej@gmail.com>
2019-04-04 16:22:54 +0200

    Add navigation scroll

--- HTML, CSS/omnifood/resources/js/scripts.js
+++ HTML, CSS/omnifood/resources/js/scripts.js
@@ -15,4 +15,33 @@ $(document).ready(function() {
   $('.js--scroll-to-start').click(function() {
     $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
   });
+
+  $('a[href*="#"]')
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) {
+            return false;
+          } else {
+            $target.attr('tabindex','-1');
+            $target.focus();
+          };
+        });
+      }
+    }
+  });
 });
