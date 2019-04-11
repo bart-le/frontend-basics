ec0a13370ff4e3ed791d7f6bf24b99b1dea38cca
Bart Le <le.bartlomiej@gmail.com>
2019-04-11 18:16:32 +0200

    Type coercion and variable mutation

--- JavaScript/Basics/script.js
+++ JavaScript/Basics/script.js
@@ -1,4 +1,5 @@
 // VARIABLES AND DATA TYPES
+/*
 var firstName = 'Bart';
 console.log(firstName);
 var lastName = 'Le';
@@ -11,3 +12,23 @@ var job;
 console.log(job);
 job = 'developer';
 console.log(job);
+*/
+// TYPE COERCION AND VARIABLE MUTATION
+var firstName = 'Bart';
+var age = 24;
+
+// // TYPE COERCION
+console.log(firstName + age);
+
+var job, isMarried;
+job = 'developer';
+isMarried = false;
+console.log(firstName + ' is a ' + age + '-year-old ' + job + '. Is he married? ' + isMarried);
+
+// // VARIABLE MUTATION
+age = 'twenty four';
+job = 'bartender';
+alert(firstName + ' is a ' + age + '-year-old ' + job + '. Is he married? ' + isMarried);
+
+var lastName = prompt('What is his last name?');
+console.log(firstName + ' ' + lastName);
