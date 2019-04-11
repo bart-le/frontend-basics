13222577e7fb01226e63dbedaf2456e2600a1bc8
Bart Le <le.bartlomiej@gmail.com>
2019-04-11 18:11:32 +0200

    Variables and data types

---
+++ JavaScript/Basics/script.js
@@ -0,0 +1,13 @@
+// VARIABLES AND DATA TYPES
+var firstName = 'Bart';
+console.log(firstName);
+var lastName = 'Le';
+
+var age = 24;
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+job = 'developer';
+console.log(job);
