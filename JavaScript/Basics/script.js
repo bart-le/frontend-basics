f37900c92ed3c30c8c8912eb325fdd95d8eaea2b
Bart Le <le.bartlomiej@gmail.com>
2019-04-11 18:20:29 +0200

    Basic operators

--- JavaScript/Basics/script.js
+++ JavaScript/Basics/script.js
@@ -14,6 +14,7 @@ job = 'developer';
 console.log(job);
 */
 // TYPE COERCION AND VARIABLE MUTATION
+/*
 var firstName = 'Bart';
 var age = 24;
 
@@ -32,3 +33,28 @@ alert(firstName + ' is a ' + age + '-year-old ' + job + '. Is he married? ' + is
 
 var lastName = prompt('What is his last name?');
 console.log(firstName + ' ' + lastName);
+*/
+// BASIC OPERATORS
+var now, yearBart, yearBill;
+now = 2019;
+ageBart = 24;
+ageBill = 64;
+
+// // MATH OPERATOR
+yearBart = now - ageBart;
+yearBill = now - ageBill;
+console.log(yearBart);
+console.log(now + 3);
+console.log(now * 3);
+console.log(now / 10);
+
+// // LOGICAL OPERATOR
+var bartOlder = ageBart > ageBill;
+console.log(bartOlder);
+
+// // TYPEOF OPERATOR
+console.log(typeof bartOlder);
+console.log(typeof ageBill);
+console.log(typeof 'Bill Gates is a co-founder of Microsoft.');
+var x;
+console.log(typeof x);
