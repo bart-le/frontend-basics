38d07421814592baef41f7c02433f7d803435ff3
Bart Le <le.bartlomiej@gmail.com>
2019-04-13 15:00:35 +0200

    Set current score update condition

--- JavaScript/DOM/app.js
+++ JavaScript/DOM/app.js
@@ -24,4 +24,9 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
 	var diceDOM = document.querySelector('.dice');
 	diceDOM.style.display = 'block';
 	diceDOM.src = 'dice-' + dice + '.png';
+
+	if (dice !== 1) {
+		roundScore += dice;
+		document.querySelector('#current-' + activePlayer).textContent = roundScore;
+	}
 });
