0858aced46e6431395064737b81589b75fa5f69a
Bart Le <le.bartlomiej@gmail.com>
2019-04-13 14:37:20 +0200

    Add dice rolling event listener

--- JavaScript/DOM/app.js
+++ JavaScript/DOM/app.js
@@ -18,3 +18,10 @@ document.getElementById('score-0').textContent = '0';
 document.getElementById('score-1').textContent = '0';
 document.getElementById('current-0').textContent = '0';
 document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+	var dice = Math.ceil(Math.random() * 6);
+	var diceDOM = document.querySelector('.dice');
+	diceDOM.style.display = 'block';
+	diceDOM.src = 'dice-' + dice + '.png';
+});
