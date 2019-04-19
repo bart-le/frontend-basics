90c010a2adab0804e34d1871d4c9c8e9c30cc263
Bart Le <le.bartlomiej@gmail.com>
2019-04-19 15:57:38 +0200

    Coding Challenge 7

--- JavaScript/Objects/script.js
+++ JavaScript/Objects/script.js
@@ -171,6 +171,7 @@ retirementUS(1995);
 retirementIceland(1995);
 */
 // BIND, CALL AND APPLY
+/*
 var bart = {
 	name: 'Bart',
 	age: 24,
@@ -206,3 +207,84 @@ bartInformal('night');
 
 var michaelFormal = bart.presentation.bind(michael, 'formal');
 michaelFormal('afternoon');
+*/
+// CODING CHALLENGE 7
+// // 1. Build a function constructor called Question to describe a question. A question should include:
+// // a) question itself,
+// // b) the answers from which the player can choose the correct one (choose an adequate data structure here, array, object, etc.),
+// // c) correct answer (I would use a number for this).
+// // 2. Create a couple of questions using the constructor.
+// // 3. Store them all inside an array.
+// // 4. Select one random question and log it on the console, together with the possible answers (each question should have a number) (HINT: write a method for the Question objects for this task).
+// // 5. Use the 'prompt' function to ask the user for the correct answer. The user should input the number of the correct answer such as you displayed it on Task 4.
+// // 6. Check if the answer is correct and print to the console whether the answer is correct ot nor (HINT: write another method for this).
+// // 7. Suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is private and doesn't interfere with the other programmers code (HINT: we learned a special technique to do exactly that).
+// // 8. After you display the result, display the next random question, so that the game never ends (HINT: write a function for this and call it right after displaying the result).
+// // 9. Be careful: after Task 8, the game literally never ends. So include the option to quit the game if the user writes 'exit' instead of the answer. In this case, DON'T call the function from task 8.
+// // 10. Track the user's score to make the game more fun! So each time an answer is correct, add 1 point to the score (HINT: I'm going to use the power of closures for this, but you don't have to, just do this with the tools you feel more comfortable at this point).
+// // 11. Display the score in the console. Use yet another method for this.
+
+(function() {
+	function Questions(question, answers, correct) {
+		this.question = question;
+		this.answers = answers;
+		this.correct = correct;
+	}
+	
+	Questions.prototype.displayQuestion = function() {
+		console.log(this.question);
+	
+		for (var i = 0; i < this.answers.length; i++) {
+			console.log(i + ': ' + this.answers[i]);
+		}
+	}
+	
+	Questions.prototype.checkAnswer = function(ans, callback) {
+		var sc;
+
+		if (ans === this.correct) {
+			console.log('Correct!');
+			sc = callback(true);
+		} else {
+			console.log('Wrong.');
+			sc = callback(false);
+		}
+
+		this.displayScore(sc);
+	}
+	
+	Questions.prototype.displayScore = function(score) {
+		console.log('Current score: ' + score);
+		console.log('---------');
+	}
+
+	var q1 = new Questions('Does he look like a bitch?', ['Yeah.', 'No.', 'What?'], 2);
+	var q2 = new Questions('Do I feel lucky? Well, do ya, punk?', ['Hey! I gots to know!', 'Nope.', 'Kinda.'], 0);
+	var q3 = new Questions('Mrs. Robinson, you\'re trying to seduce me! Aren\'t you?', ['I beg your pardon?', 'Well, no.', 'Oh yes, I am.'], 1);
+	
+	var questions = [q1, q2, q3];
+	
+	function score() {
+		var sc = 0;
+		return function(correct) {
+			if (correct) {
+				sc++;
+			}
+			return sc;
+		}
+	}
+	var keepScore = score();
+
+	function nextQuestion() {
+		var n = Math.floor(Math.random() * questions.length);
+		questions[n].displayQuestion();
+		
+		var answer = prompt('The answer is:');
+		
+		if (answer !== 'exit') {
+			questions[n].checkAnswer(parseInt(answer), keepScore);
+			nextQuestion();
+		}
+	}
+	nextQuestion();
+})();
