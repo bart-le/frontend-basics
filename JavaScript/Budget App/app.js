5bbaa50859c1bebe5722c577f966706fef0e32d8
Bart Le <le.bartlomiej@gmail.com>
2019-04-20 13:16:33 +0200

    Organise code

--- JavaScript/Budget App/app.js
+++ JavaScript/Budget App/app.js
@@ -25,17 +25,27 @@ var UIController = (function() {
 })();
 
 var controller = (function(budgetCtrl, UICtrl) {
-	var DOM = UICtrl.getDOMstrings();
+	var setupEventListeners = function() {
+		var DOM = UICtrl.getDOMstrings();
+
+		document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+		document.addEventListener('keypress', function(event) {
+			if (event.keyCode === 13 || event.which === 13) {
+				ctrlAddItem();
+			}
+		});
+	};
 	
 	var ctrlAddItem = function() {
 		var input = UICtrl.getInput();
-	}
+	};
 
-	document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
-
-	document.addEventListener('keypress', function(event) {
-		if (event.keyCode === 13 || event.which === 13) {
-			ctrlAddItem();
+	return {
+		init: function() {
+			setupEventListeners();
 		}
-	});
+	};
 })(budgetController, UIController);
+
+controller.init();
