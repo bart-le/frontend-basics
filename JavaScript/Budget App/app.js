68b629200b9da1eb5bd1c2031d1ab03944811b1d
Bart Le <le.bartlomiej@gmail.com>
2019-04-20 13:02:14 +0200

    Store DOM classes in object

--- JavaScript/Budget App/app.js
+++ JavaScript/Budget App/app.js
@@ -3,23 +3,35 @@ var budgetController = (function() {
 })();
 
 var UIController = (function() {
+	var DOMstrings = {
+		inputType: '.add__type',
+		inputDescription: '.add__description',
+		inputValue: '.add__value',
+		inputBtn: '.add__btn'
+	};
+	
 	return {
 		getInput: function() {
 			return {
-				type: document.querySelector('.add__type').value,
-				description: document.querySelector('.add__description').value,
-				value: document.querySelector('.add__value').value
+				type: document.querySelector(DOMstrings.inputType).value,
+				description: document.querySelector(inputDescription).value,
+				value: document.querySelector(inputValue).value
 			};
+		},
+		getDOMstrings: function() {
+			return DOMstrings;
 		}
 	};
 })();
 
 var controller = (function(budgetCtrl, UICtrl) {
+	var DOM = UICtrl.getDOMstrings();
+	
 	var ctrlAddItem = function() {
 		var input = UICtrl.getInput();
 	}
 
-	document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+	document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
 	document.addEventListener('keypress', function(event) {
 		if (event.keyCode === 13 || event.which === 13) {
