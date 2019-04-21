6197c944ceacd465392f7f025a68ef232045db18
Bart Le <le.bartlomiej@gmail.com>
2019-04-21 19:43:19 +0200

    Format displayed numbers

--- JavaScript/Budget App/app.js
+++ JavaScript/Budget App/app.js
@@ -126,6 +126,22 @@ var UIController = (function() {
 		container: '.container',
 		expensesPercLabel: '.item__percentage'
 	};
+
+	
+	var formatNumber = function(num, type) {
+		var numSplit, int, dec;
+		num = Math.abs(num);
+		num = num.toFixed(2);
+
+		numSplit = num.split('.');
+		int = numSplit[0];
+		dec = numSplit[1];
+		
+		if (int.length > 3) {
+			int = int.substr(0, int.length - 3) + ',' + int.substr(int.length - 3, int.length);
+		}
+		return (type === 'inc' ? '+' : '-') + ' ' + int + '.' + dec;
+	};
 	
 	return {
 		getInput: function() {
@@ -150,7 +166,7 @@ var UIController = (function() {
 			}
 
 			newHTML = html.replace('%description%', obj.description);
-			newHTML = newHTML.replace('%value%', obj.value);
+			newHTML = newHTML.replace('%value%', formatNumber(obj.value, type));
 			newHTML = newHTML.replace('%id%', obj.id);
 			document.querySelector(element).insertAdjacentHTML('beforeend', newHTML);
 		},
@@ -167,9 +183,12 @@ var UIController = (function() {
 			fields[0].focus();
 		},
 		displayBudget: function(obj) {
-			document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
-			document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
-			document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+			var type;
+			obj.budget > 0 ? type = 'inc' : type = 'exp';
+
+			document.querySelector(DOMstrings.budgetLabel).textContent = formatNumber(obj.budget, type);
+			document.querySelector(DOMstrings.incomeLabel).textContent = formatNumber(obj.totalInc, 'inc');
+			document.querySelector(DOMstrings.expensesLabel).textContent = formatNumber(obj.totalExp, 'exp');
 			if (obj.percentage > 0) {
 				document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';
 			} else {
