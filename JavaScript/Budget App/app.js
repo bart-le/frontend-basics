fa88a68fe6c9a676f56ac14b008a777e4dfd42e2
Bart Le <le.bartlomiej@gmail.com>
2019-04-21 13:02:59 +0200

    Display items in UI

--- JavaScript/Budget App/app.js
+++ JavaScript/Budget App/app.js
@@ -81,7 +81,11 @@ var UIController = (function() {
 		inputValue: '.add__value',
 		inputBtn: '.add__btn',
 		incomesContainer: '.income__list',
-		expensesContainer: '.expenses__list'
+		expensesContainer: '.expenses__list',
+		budgetLabel: '.budget__value',
+		incomeLabel: '.budget__income--value',
+		expensesLabel: '.budget__expenses--value',
+		percentageLabel: '.budget__expenses--percentage'
 	};
 	
 	return {
@@ -122,6 +126,16 @@ var UIController = (function() {
 			});
 
 			fields[0].focus();
+		},
+		displayBudget: function(obj) {
+			document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+			document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+			document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+			if (obj.percentage > 0) {
+				document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';
+			} else {
+				document.querySelector(DOMstrings.percentageLabel).textContent = '---';
+			}
 		}
 	};
 })();
@@ -142,7 +156,7 @@ var controller = (function(budgetCtrl, UICtrl) {
 	var updateBudget = function() {
 		budgetCtrl.calculateBudget();
 		var budget = budgetCtrl.getBudget();
-		console.log(budget);
+		UICtrl.displayBudget(budget);
 	}
 
 	var ctrlAddItem = function() {
@@ -164,6 +178,13 @@ var controller = (function(budgetCtrl, UICtrl) {
 	return {
 		init: function() {
 			setupEventListeners();
+			UICtrl.displayBudget({
+				budget: 0,
+				totalInc: 0,
+				totalExp: 0,
+				percentage: 0
+			});
+
 		}
 	};
 })(budgetController, UIController);
