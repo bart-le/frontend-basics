98c69d91212cacd3f9d37efa8ce20cca72feb8b2
Bart Le <le.bartlomiej@gmail.com>
2019-04-21 22:34:36 +0200

    Var, let and const

---
+++ JavaScript/ES6+/script.js
@@ -0,0 +1,45 @@
+// VAR, LET AND CONST
+
+// // DECLARATION
+var name5 = 'Jane Smith';
+var age5 = 23;
+name5 = 'Jane Miller';
+console.log(name5);
+
+const name6 = 'Jane Smith';
+let age6 = 23;
+name6 = 'Jane Miller';
+console.log(name6);
+
+// // FUNCTIONAL VS. BLOCK SCOPE
+function driversLicence5(passedTest) {
+	if (passedTest) {
+		console.log(firstName);
+		var firstName = 'Bart';
+		var yearOfBirth = 1995;
+	}
+
+	console.log(firstName + ', born in ' + yearOfBirth + ', is now officially allowed to drive a car.');
+}
+driversLicence5(true);
+
+function driversLicence6(passedTest) {
+	console.log(firstName);
+	let firstName;
+	const yearOfBirth = 1995;
+	
+	if (passedTest) {
+		firstName = 'Bart';
+	}
+	
+	console.log(firstName + ', born in ' + yearOfBirth + ', is now officially allowed to drive a car.');
+}
+driversLicence6(true);
+
+var i = 23;
+
+for (var i = 0; i < 5; i++) {
+	console.log(i);
+}
+
+console.log(i);
