5f91a273078f3c9ae5efa02f89b89ddcfdc6c320
Bart Le <le.bartlomiej@gmail.com>
2019-04-22 14:57:03 +0200

    Arrow functions lexical this

--- JavaScript/ES6+/script.js
+++ JavaScript/ES6+/script.js
@@ -84,6 +84,7 @@ console.log(n.includes('art'));
 console.log(`${firstName} `.repeat(3));
 */
 // ARROW FUNCTIONS
+/*
 const years = [1990, 1995, 1945, 1969];
 
 // // ES5
@@ -105,3 +106,65 @@ ages6 = years.map((el, index) => {
 	return `Age element ${index + 1}: ${age}.`;
 });
 console.log(ages6);
+*/
+// ARROW FUNCTIONS LEXICAL THIS
+// // ES5 OBJECT FUNCTION
+var box5 = {
+	color: 'green',
+	position: 1,
+	clickMe: function() {
+		var self = this;
+		document.querySelector('.green').addEventListener('click', function() {
+			var str = 'This is box number ' + self.position + ' and it is ' + self.color;
+			alert(str);
+		});
+	}
+};
+// box5.clickMe();
+
+// // ES6 OBJECT FUNCTION
+const box6 = {
+	color: 'green',
+	position: 1,
+	clickMe: function() {
+		document.querySelector('.green').addEventListener('click', () => {
+			let str = 'This is box number ' + this.position + ' and it is ' + this.color;
+			alert(str);
+		});
+	}
+};
+// box6.clickMe();
+
+const box66 = {
+	color: 'green',
+	position: 1,
+	clickMe: () => {
+		document.querySelector('.green').addEventListener('click', () => {
+			let str = 'This is box number ' + this.position + ' and it is ' + this.color;
+			alert(str);
+		});
+	}
+};
+// box66.clickMe();
+
+function Person(name) {
+	this.name = name;
+}
+
+var friends = ['Bill', 'Steve', 'Michael'];
+
+// // ES5 CONSTRUCTOR FUNCTION THIS
+Person.prototype.myFriends5 = function(friends) {
+	var arr = friends.map(function(el) {
+		return this.name + ' is friends with ' + el;
+	}.bind(this));
+	console.log(arr);
+};
+new Person('Bart').myFriends5(friends);
+
+// // ES6 CONSTRUCTOR FUNCTION THIS
+Person.prototype.myFriends6 = function(friends) {
+	var arr = friends.map(el => `${this.name} is friends with ${el}`);
+	console.log(arr);
+};
+new Person('Elon').myFriends6(friends);
