b75bb8b0b61191e892bed3e8488e3f0d82e56390
Bart Le <le.bartlomiej@gmail.com>
2019-04-23 03:58:09 +0200

    Classes

--- JavaScript/ES6+/script.js
+++ JavaScript/ES6+/script.js
@@ -337,6 +337,7 @@ var james = new Person6('James', 1968);
 console.log(james);
 */
 // MAPS
+/*
 const question = new Map();
 question.set('question', 'Who\'s the current owner of Charlotte Hornets?');
 question.set(1, 'Shaquille O\'Neal');
@@ -369,3 +370,39 @@ for (let [key, value] of question.entries()) {
 
 const ans = parseInt(prompt('The answer is:'));
 console.log(question.get(ans === question.get('correct')));
+*/
+// CLASSES
+// // ES5
+var Person5 = function(name, yearOfBirth, job) {
+	this.name = name;
+	this.yearOfBirth = yearOfBirth;
+	this.job = job;
+}
+
+Person5.prototype.calculateAge = function() {
+	var age = new Date().getFullYear() - this.yearOfBirth;
+	console.log(age);
+}
+
+var bart5 = new Person5('Bart', 1995, 'developer');
+
+// // ES6
+class Person6 {
+	constructor (name, yearOfBirth, job) {
+		this.name = name;
+		this.yearOfBirth = yearOfBirth;
+		this.job = job;
+	}
+	
+	calculateAge() {
+		var age = new Date().getFullYear() - this.yearOfBirth;
+		console.log(age);
+	}
+	
+	static greeting() {
+		console.log('Hello there!');
+	}
+}
+
+const bart6 = new Person6('Bart', 1995, 'developer');
+Person6.greeting();
