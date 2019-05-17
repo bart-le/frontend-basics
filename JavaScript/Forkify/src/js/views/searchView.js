44b21114a71be40437093d96ef3cc2cb5154c280
Bart Le <le.bartlomiej@gmail.com>
2019-05-18 01:47:06 +0200

    Add pagination

--- JavaScript/Forkify/src/js/views/searchView.js
+++ JavaScript/Forkify/src/js/views/searchView.js
@@ -8,6 +8,7 @@ export const clearInput = () => {
 
 export const clearResults = () => {
 	elements.searchResList.innerHTML = '';
+	elements.searchResPages.innerHTML = '';
 };
 
 const limitRecipeTitle = (title, limit = 17) => {
@@ -39,6 +40,40 @@ const renderRecipe = recipe => {
 	elements.searchResList.insertAdjacentHTML('beforeend', markup);
 };
 
-export const renderResults = recipes => {
-	recipes.forEach(renderRecipe);
+// TYPE: PREV/NEXT
+const createButton = (page, type) => `
+	<button class="btn-inline results__btn--${type}" data-goto=${type === 'prev' ? page - 1 : page + 1}>
+		<span>Page ${type === 'prev' ? page - 1 : page + 1}</span>
+		<svg class="search__icon">
+			<use href="img/icons.svg#icon-triangle-${type === 'prev' ? 'left' : 'right'}"></use>
+		</svg>
+	</button>
+`;
+
+const renderButtons = (page, numResults, resPerPage) => {
+	const pages = Math.ceil(numResults / resPerPage);
+
+	let button;
+	if (page === 1 && pages > 1) {
+		// SINGLE NEXT PAGE BUTTON
+		button = createButton(page, 'next');
+	} else if (page < pages) {
+		// BOTH BUTTONS
+		button = `${createButton(page, 'prev')}${createButton(page, 'next')}`;
+	} else if (page === pages && pages > 1) {
+		// SINGLE PREVIOUS PAGE BUTTON
+		button = createButton(page, 'prev');
+	}
+
+	elements.searchResPages.insertAdjacentHTML('afterbegin', button);
+};
+
+export const renderResults = (recipes, page = 1, resPerPage = 10) => {
+	// RENDER CURRENT PAGE RESULTS
+	const start = (page - 1) * resPerPage;
+	const end = page * resPerPage;
+	recipes.slice(start, end).forEach(renderRecipe);
+
+	// RENDER PAGINATION BUTTONS
+	renderButtons(page, recipes.length, resPerPage);
 };
