ce76f4fc8d6cb0575c98abf44222a0f26fcde8da
Bart Le <le.bartlomiej@gmail.com>
2019-05-31 01:58:34 +0200

    Build shopping list model

--- JavaScript/Forkify/src/js/index.js
+++ JavaScript/Forkify/src/js/index.js
@@ -1,5 +1,6 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
+import List from './models/List';
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
 import { elements, renderLoader, clearLoader } from "./views/base";
@@ -71,7 +72,6 @@ const controlRecipe = async () => {
 		try {
 			// GET RECIPE DATA
 			await state.recipe.getRecipe();
-			console.log(state.recipe.ingredients);
 			state.recipe.parseIngredients();
 			// CALCULATE SERVINGS AND TIME
 			state.recipe.calcTime();
@@ -99,5 +99,4 @@ elements.recipe.addEventListener('click', e => {
 		state.recipe.updateServings('inc');
 		recipeView.updateServingsIngredients(state.recipe);
 	}
-	console.log(state.recipe);
 });
