db8942a396f16fa7f716bfd8e3294f217a5546bc
Bart Le <le.bartlomiej@gmail.com>
2019-06-04 23:37:26 +0200

    Trigger GET requests

--- React/pics/src/components/App.js
+++ React/pics/src/components/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
 	onSearchSubmit(term) {
-		console.log(term);
+		axios.get('https://api.unsplash.com/search/photos', {
+			params: { query: term },
+			headers: {
+				Authorization: 'Client-ID c24d146ddd71a53a965d1b64707549e8cd985c939dc46273efbde7a6a2f270a0'
+			}
+		});
 	}
 
 	render() {
