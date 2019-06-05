1419cabdbfa55ebc56e19836618a1856e8cbd020
Bart Le <le.bartlomiej@gmail.com>
2019-06-05 19:44:23 +0200

    Set state and bind this with arrow function

--- React/pics/src/components/App.js
+++ React/pics/src/components/App.js
@@ -3,7 +3,9 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-	async onSearchSubmit(term) {
+	state = { images: [] };
+
+	onSearchSubmit = async term => {
 		const response = await axios
 			.get('https://api.unsplash.com/search/photos', {
 				params: { query: term },
@@ -11,14 +13,14 @@ class App extends React.Component {
 					Authorization: 'Client-ID c24d146ddd71a53a965d1b64707549e8cd985c939dc46273efbde7a6a2f270a0'
 			}
 		});
-
-		console.log(response.data.results);
+		this.setState({ images: response.data.results });
 	}
 
 	render() {
 		return (
 			<div className="ui container" style={{ marginTop: '10px' }}>
 				<SearchBar onSubmit={ this.onSearchSubmit } />
+				Found: { this.state.images.length } images
 			</div>
 		);
 	}
