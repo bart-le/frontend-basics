0b32ec89ff4b3aa0ac23bc89fb6e7ec2fb9a181f
Bart Le <le.bartlomiej@gmail.com>
2019-06-05 19:27:43 +0200

    Refractor onSearchSubmit method to async/await

--- React/pics/src/components/App.js
+++ React/pics/src/components/App.js
@@ -3,13 +3,16 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-	onSearchSubmit(term) {
-		axios.get('https://api.unsplash.com/search/photos', {
-			params: { query: term },
-			headers: {
-				Authorization: 'Client-ID c24d146ddd71a53a965d1b64707549e8cd985c939dc46273efbde7a6a2f270a0'
+	async onSearchSubmit(term) {
+		const response = await axios
+			.get('https://api.unsplash.com/search/photos', {
+				params: { query: term },
+				headers: {
+					Authorization: 'Client-ID c24d146ddd71a53a965d1b64707549e8cd985c939dc46273efbde7a6a2f270a0'
 			}
 		});
+
+		console.log(response.data.results);
 	}
 
 	render() {
