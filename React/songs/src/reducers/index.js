09e8b7729a5d649fd6cee36e0167bea504d58f5b
Bart Le <le.bartlomiej@gmail.com>
2019-06-26 17:28:11 +0200

    Build reducer

---
+++ React/songs/src/reducers/index.js
@@ -0,0 +1,16 @@
+const songsReducer = () => {
+	return [
+		{ title: 'All Star', duration: '3:21' },
+		{ title: 'I Want It That Way', duration: '3:33' },
+		{ title: 'Africa', duration: '4:55' },
+		{ title: 'Never Gonna Give You Up', duration: '3:35' }
+	];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+	if (action.type === 'SONG_SELECTED') {
+		return action.payload;
+	}
+
+	return selectedSong;
+};
