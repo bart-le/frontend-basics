0ef5723e7f64feaae0d5c1a5cec0de573bb4d16d
Bart Le <le.bartlomiej@gmail.com>
2019-10-10 19:03:33 +0200

    Refactor Sorter into an abstract parent class

--- TypeScript/sort/src/index.ts
+++ TypeScript/sort/src/index.ts
@@ -1,22 +1,18 @@
 import { CharactersCollection } from './CharactersCollection';
 import { LinkedList } from './LinkedList';
 import { NumbersCollection } from './NumbersCollection';
-import { Sorter } from './Sorter';
 
 const numbersCollection = new NumbersCollection([4, 2, 0, 6, 9]);
-const sorter = new Sorter(numbersCollection);
-sorter.sort();
+numbersCollection.sort();
 console.log(numbersCollection.data);
 
 const charactersCollection = new CharactersCollection('TypeScript');
-const sorter2 = new Sorter(charactersCollection);
-sorter2.sort();
+charactersCollection.sort();
 console.log(charactersCollection.data);
 
 const linkedList = new LinkedList();
 linkedList.add(23);
 linkedList.add(-500);
 linkedList.add(31);
-const sorter3 = new Sorter(linkedList);
-sorter3.sort();
+linkedList.sort();
 linkedList.print();
