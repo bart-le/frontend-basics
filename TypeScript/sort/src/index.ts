f3798adc2a6ea4d02a76cbac9622c238dcb57d4c
Bart Le <le.bartlomiej@gmail.com>
2019-10-10 17:17:09 +0200

    Create class for strings

--- TypeScript/sort/src/index.ts
+++ TypeScript/sort/src/index.ts
@@ -1,3 +1,4 @@
+import { CharactersCollection } from './CharactersCollection';
 import { NumbersCollection } from './NumbersCollection';
 import { Sorter } from './Sorter';
 
@@ -5,3 +6,8 @@ const numbersCollection = new NumbersCollection([4, 2, 0, 6, 9]);
 const sorter = new Sorter(numbersCollection);
 sorter.sort();
 console.log(numbersCollection.data);
+
+const charactersCollection = new CharactersCollection('TypeScript');
+const sorter2 = new Sorter(charactersCollection);
+sorter2.sort();
+console.log(charactersCollection.data);
