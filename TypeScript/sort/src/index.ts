136a72a5c664f586dc01bd71d8bb5b741ed848a5
Bart Le <le.bartlomiej@gmail.com>
2019-10-10 16:32:50 +0200

    Separate swap and compare methods

--- TypeScript/sort/src/index.ts
+++ TypeScript/sort/src/index.ts
@@ -1,24 +1,7 @@
-class Sorter {
-	constructor(public collection: number[] | string) {}
+import { NumbersCollection } from './NumbersCollection';
+import { Sorter } from './Sorter';
 
-	sort(): void {
-		const { length } = this.collection;
-
-		for (let i = 0; i < length; i++) {
-			for (let j = 0; j < length; j++) {
-				if (this.collection instanceof Array) {
-					if (this.collection[j] > this.collection[j + 1]) {
-						[this.collection[j], this.collection[j + 1]] = [this.collection[j + 1], this.collection[j]];
-					}
-				}
-
-				if (typeof this.collection === 'string') {
-				}
-			}
-		}
-	}
-}
-
-const sorter = new Sorter([4, 2, 0, 6, 9]);
+const numbersCollection = new NumbersCollection([4, 2, 0, 6, 9]);
+const sorter = new Sorter(numbersCollection);
 sorter.sort();
-console.log(sorter.collection);
+console.log(numbersCollection.data);
