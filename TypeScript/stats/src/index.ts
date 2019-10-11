ba1c070365e875f3b9a55d9cb4f8972089e76a29
Bart Le <le.bartlomiej@gmail.com>
2019-10-11 23:55:37 +0200

    Create HTML report generator

--- TypeScript/stats/src/index.ts
+++ TypeScript/stats/src/index.ts
@@ -1,12 +1,12 @@
 import { WinsAnalysis } from './analyzers/WinsAnalysis';
 import { CsvFileReader } from './CsvFileReader';
 import { MatchReader } from './MatchReader';
-import { ConsoleReport } from './reportTargets/ConsoleReport';
+import { HtmlReport } from './reportTargets/HtmlReport';
 import { Summary } from './Summary';
 
 const csvFileReader = new CsvFileReader('football.csv');
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
 
-const summary = new Summary(new WinsAnalysis('Chelsea'), new ConsoleReport());
+const summary = new Summary(new WinsAnalysis('Chelsea'), new HtmlReport());
 summary.buildAndPrintReport(matchReader.matches);
