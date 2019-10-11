b6e72e68d8f55fb967fe0d2ccdcdcbdd862ea7d8
Bart Le <le.bartlomiej@gmail.com>
2019-10-11 21:48:49 +0200

    Refactor from inheritance to composition

--- TypeScript/stats/src/inheritance/index.ts
+++ TypeScript/stats/src/inheritance/index.ts
@@ -1,13 +1,11 @@
-import { CsvFileReader } from './CsvFileReader';
 import { MatchReader } from './MatchReader';
 import { MatchResults } from './MatchResults';
 
-const csvFileReader = new CsvFileReader('football.csv');
-const matchReader = new MatchReader(csvFileReader);
-matchReader.load();
+const reader = new MatchReader('football.csv');
+reader.read();
 
 let manUtdWins = 0;
-for (let match of matchReader.matches) {
+for (let match of reader.data) {
 	if (
 		(match[1] === 'Man United' && match[5] === MatchResults.HomeWin) ||
 		(match[2] === 'Man United' && match[5] === MatchResults.AwayWin)
