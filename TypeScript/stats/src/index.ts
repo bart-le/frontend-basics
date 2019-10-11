1c3790ddb401a88d6d876a19122abce9a9ae83c3
Bart Le <le.bartlomiej@gmail.com>
2019-10-11 23:42:56 +0200

    Call buildAndPrintReport method

--- TypeScript/stats/src/index.ts
+++ TypeScript/stats/src/index.ts
@@ -1,6 +1,12 @@
+import { WinsAnalysis } from './analyzers/WinsAnalysis';
 import { CsvFileReader } from './CsvFileReader';
 import { MatchReader } from './MatchReader';
+import { ConsoleReport } from './reportTargets/ConsoleReport';
+import { Summary } from './Summary';
 
 const csvFileReader = new CsvFileReader('football.csv');
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
+
+const summary = new Summary(new WinsAnalysis('Chelsea'), new ConsoleReport());
+summary.buildAndPrintReport(matchReader.matches);
