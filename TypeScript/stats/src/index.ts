ce255f54c86286e49cc75958ec7edca733eef36a
Bart Le <le.bartlomiej@gmail.com>
2019-10-12 00:13:35 +0200

    Replace instances with static methods

--- TypeScript/stats/src/index.ts
+++ TypeScript/stats/src/index.ts
@@ -1,12 +1,8 @@
-import { WinsAnalysis } from './analyzers/WinsAnalysis';
-import { CsvFileReader } from './CsvFileReader';
 import { MatchReader } from './MatchReader';
-import { HtmlReport } from './reportTargets/HtmlReport';
 import { Summary } from './Summary';
 
-const csvFileReader = new CsvFileReader('football.csv');
-const matchReader = new MatchReader(csvFileReader);
+const matchReader = MatchReader.fromCsv('football.csv');
 matchReader.load();
 
-const summary = new Summary(new WinsAnalysis('Chelsea'), new HtmlReport());
+const summary = Summary.winsAnalysisWithHtmlReport('Chelsea');
 summary.buildAndPrintReport(matchReader.matches);
