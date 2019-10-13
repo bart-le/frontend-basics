897af27a45a43a8498b4cc52cff09d25e9780fea
Bart Le <le.bartlomiej@gmail.com>
2019-10-13 20:47:02 +0200

    Send request

--- Node.js/basics/code/app.js
+++ Node.js/basics/code/app.js
@@ -1,7 +1,19 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-	console.log(req);
+	console.log(req.method, req.url, req.headers);
+	res.setHeader('Content-Type', 'text/html');
+	res.write('<html>');
+	res.write(`
+		<head>
+			<title>My first Node server</title>
+		</head>
+		<body>
+			<h1>Hello from my Node.js server!</h1>
+		</body>
+	`);
+	res.write('</html>');
+	res.end();
 });
 
 server.listen(3000);
