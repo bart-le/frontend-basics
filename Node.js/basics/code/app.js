a950d824aeb0d3e7a757314b098262bf1d7916b3
Bart Le <le.bartlomiej@gmail.com>
2019-10-17 00:57:51 +0200

    Parse request bodies

--- Node.js/basics/code/app.js
+++ Node.js/basics/code/app.js
@@ -22,7 +22,16 @@ const server = http.createServer((req, res) => {
 		return res.end();
 	}
 	if (url === '/message' && method === 'POST') {
-		fs.writeFileSync('message.txt', 'General Kenobi.');
+		const body = [];
+		req.on('data', chunk => {
+			console.log(chunk);
+			body.push(chunk);
+		});
+		req.on('close', () => {
+			const parsedBody = Buffer.concat(body).toString();
+			const message = parsedBody.split('=')[1];
+			fs.writeFileSync('message.txt', message);
+		});
 		res.statusCode = 302;
 		res.setHeader('Location', '/');
 		return res.end();
