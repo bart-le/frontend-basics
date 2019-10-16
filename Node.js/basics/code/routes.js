c50a935778a95036eb407fffaece962e553edff1
Bart Le <le.bartlomiej@gmail.com>
2019-10-17 01:39:36 +0200

    Use Node modules system

---
+++ Node.js/basics/code/routes.js
@@ -0,0 +1,53 @@
+const fs = require('fs');
+
+const requestHandler = (req, res) => {
+	const url = req.url;
+	const method = req.method;
+
+	if (url === '/') {
+		res.write('<html>');
+		res.write(`
+			<head>
+				<title>My first Node server</title>
+			</head>
+			<body>
+				<form action="/message" method="post">
+					<input type="text" name="message">
+					<button type="submit">Send</button>
+				</form>
+			</body>
+		`);
+		res.write('</html>');
+		return res.end();
+	}
+	if (url === '/message' && method === 'POST') {
+		const body = [];
+		req.on('data', chunk => {
+			console.log(chunk);
+			body.push(chunk);
+		});
+		return req.on('close', () => {
+			const parsedBody = Buffer.concat(body).toString();
+			const message = parsedBody.split('=')[1];
+			fs.writeFile('message.txt', message, error => {
+				res.statusCode = 302;
+				res.setHeader('Location', '/');
+				return res.end();
+			});
+		});
+	}
+	res.setHeader('Content-Type', 'text/html');
+	res.write('<html>');
+	res.write(`
+		<head>
+			<title>My first Node server</title>
+		</head>
+		<body>
+			<h1>Hello from my Node.js server!</h1>
+		</body>
+	`);
+	res.write('</html>');
+	res.end();
+};
+
+module.exports = requestHandler;
